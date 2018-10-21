d6aaa99ad1a846457269d2ebab7178cd0d5eeab6
Shuo Wang <gken90@gmail.com>
2018-10-21 01:46:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,17 +3,7 @@
 # A song should be initialized with a name, an artist, and a genre, and be saved in the @@all array.
 
 class Song
-  attr_reader :name, :artist, :genre
-  @@all = []
-  def initialize (name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre
-    @@all << self
-  end
-  def self.all
-    @@all
-  end
+
 
 
 end
