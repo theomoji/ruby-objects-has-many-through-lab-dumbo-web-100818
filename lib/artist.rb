75fcb8ce5a245f1d39a66d0269ce8f4f951316f4
Shuo Wang <gken90@gmail.com>
2018-10-21 02:18:55 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,24 +1,8 @@
-
-# **The `Artist` model:**
-#
-# check- The `Artist` class needs a class variable `@@all` that begins as an empty array
-# - The `Artist` class needs a class method `.all` that lists each artist in the
-#   class variable
-# - An artist is initialized with a name and is saved in the `@@all` array.
-# - The `Artist` class needs an instance method, `#new_song`, that takes in an
-#   argument of a name and genre creates a new song. That song should know that it
-#   belongs to the artist.
-# - The `Artist` class needs an instance method, `#songs`, that iterates through all
-#   songs and finds the songs that belong to that artist. Try using `select` to
-#   achieve this.
-# - The `Artist` class needs an instance method, `#genres` that iterates over that
-#   artist's songs and collects the genre of each song.
-#started at 9:36pm
-
-require 'pry'
 class Artist
-  attr_reader :name
+  attr_accessor :name
+
   @@all = []
+
   def initialize(name)
     @name = name
     @@all << self
@@ -33,12 +17,10 @@ class Artist
   end
 
   def songs
-    Song.all.select {|song| song.artist == self}
+    Song.all.select { |song| song.artist == self }
   end
 
-  # def genres
-  #   songs.map{|song|song.genre}
-  # end
-
-
+  def genres
+    songs.map(&:genre)
+  end
 end
